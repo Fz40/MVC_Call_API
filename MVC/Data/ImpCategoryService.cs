c5d403a28fb1e8be9fc936e6896006dc1e21666e
Fz40 <ftet1sds@gmail.com>
2021-01-04 15:33:31 +0700

    Test Commit

--- MVC/Data/ImpCategoryService.cs
+++ MVC/Data/ImpCategoryService.cs
@@ -40,7 +40,7 @@ namespace MVC.Data
                 var res = client.GetAsync(ConfigurationManager.AppSettings["CategoryApiUrl"]);
                 res.Wait();
                 var readdata = res.Result;
-
+                //Test 
                 if (readdata.IsSuccessStatusCode)
                 {
                     var resulte = readdata.Content.ReadAsAsync<IList<CategoryModel>>();
